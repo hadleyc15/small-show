02a2dc96ceb2843565ae487c96c2f18835dd2fad
siwel20 <plthrock@gmail.com>
2020-10-03 22:15:13 -0600

    added Profile to the nav bar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -34,6 +34,11 @@ function Nav() {
               Login
             </Link>
           </li>
+          <li className="mx-1">
+            <Link to="/profile">
+              Profile
+            </Link>
+          </li>
         </ul>
       );
     }

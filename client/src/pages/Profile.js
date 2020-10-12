fd838930f211769b49ff66bef2838c3c3dfb1669
siwel20 <plthrock@gmail.com>
2020-10-11 18:59:28 -0600

    updated profile link to show only when logged in and added return to home page link on the profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -11,61 +12,142 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 function Profile() {
-    const classes = useStyles();
-    return (
-        <div className="container">
-        <form className={classes.root} noValidate autoComplete="off">
-        <div>
-          <TextField
-            required
-            id="outlined-required"
-            label="Required"
-            defaultValue="Placeholder Username"
-            variant="outlined"
-          />
-          <TextField
-            disabled
-            id="outlined-disabled"
-            label="Disabled"
-            defaultValue="Hello World"
-            variant="outlined"
-          />
-          <TextField
-            id="outlined-password-input"
-            label="Password"
-            type="password"
-            autoComplete="current-password"
-            variant="outlined"
-          />
-          <TextField
-            id="outlined-read-only-input"
-            label="Read Only"
-            defaultValue="Do we need a read only text box?"
-            InputProps={{
-              readOnly: true,
-            }}
-            variant="outlined"
-          />
-          <TextField
-            id="outlined-number"
-            label="Number"
-            type="number"
-            InputLabelProps={{
-              shrink: true,
-            }}
-            variant="outlined"
-          />
-          <TextField id="outlined-search" label="Search field" type="search" variant="outlined" />
-          <TextField
-            id="outlined-helperText"
-            label="Helper text"
-            defaultValue="Default Value"
-            helperText="Some important text"
-            variant="outlined"
+  const classes = useStyles();
+  return (
+    <div className="container my-1">
+      <Link to="/">
+        ← Go to Home
+      </Link>
+
+      <h2>Profile Page</h2>
+      {/* <form onSubmit={handleFormSubmit}> */}
+        <div className="flex-row space-between my-2">
+        {/* <p>{item.name}</p>
+        <p>{name}</p>
+        <p>{firstName}</p>
+        <p>{lastName}</p> */}
+          <label htmlFor="firstName">First Name*:</label>
+          <input
+            placeholder="First"
+            name="firstName"
+            type="firstName"
+            id="firstName"
+            // onChange={handleChange}
           />
         </div>
-      </form>
-      </div>
-    )
+        <div className="flex-row space-between my-2">
+          <label htmlFor="lastName">Last Name*:</label>
+          <input
+            placeholder="Last"
+            name="lastName"
+            type="lastName"
+            id="lastName"
+            // onChange={handleChange}
+          />
+        </div>
+
+        <div className="flex-row space-between my-2">
+          <label htmlFor="userName">Username*:</label>
+          <input
+            placeholder="Username"
+            name="userName"
+            type="userName"
+            id="userName"
+            // onChange={handleChange}
+          />
+        </div>
+        <div className="flex-row space-between my-2">
+          <label htmlFor="twitchUserName">Twitch Username:</label>
+          <input
+            placeholder="Twitch Username"
+            name="twitchUserName"
+            type="twitchUserName"
+            id="twitchUserName"
+            // onChange={handleChange}
+          />
+        </div>
+
+        <div className="flex-row space-between my-2">
+          <label htmlFor="email">Email*:</label>
+          <input
+            placeholder="youremail@test.com"
+            name="email"
+            type="email"
+            id="email"
+            // onChange={handleChange}
+          />
+        </div>
+        <div className="flex-row space-between my-2">
+          <label htmlFor="pwd">Password*:</label>
+          <input
+            placeholder="Password"
+            name="password"
+            type="password"
+            id="pwd"
+            // onChange={handleChange}
+          />
+        </div>
+        <div className="flex-row flex-end">
+          <button type="submit">
+            Submit
+          </button>
+        </div>
+      {/* </form> */}
+    </div>
+  // return (
+  //   <div className="container">
+  //     <form className={classes.root} noValidate autoComplete="off">
+  //       <div>
+  //         <TextField
+  //           required
+  //           id="outlined-required"
+  //           label="Required"
+  //           defaultValue="Placeholder Username"
+  //           variant="outlined"
+  //         />
+  //         <TextField
+  //           disabled
+  //           id="outlined-disabled"
+  //           label="Disabled"
+  //           defaultValue="Hello World"
+  //           variant="outlined"
+  //         />
+  //         <TextField
+  //           id="outlined-password-input"
+  //           label="Password"
+  //           type="password"
+  //           autoComplete="current-password"
+  //           variant="outlined"
+  //         />
+  //         <TextField
+  //           id="outlined-read-only-input"
+  //           label="Read Only"
+  //           defaultValue="Do we need a read only text box?"
+  //           InputProps={{
+  //             readOnly: true,
+  //           }}
+  //           variant="outlined"
+  //         />
+  //         <TextField
+  //           id="outlined-number"
+  //           label="Number"
+  //           type="number"
+  //           InputLabelProps={{
+  //             shrink: true,
+  //           }}
+  //           variant="outlined"
+  //         />
+  //         <TextField id="outlined-search" label="Search field" type="search" variant="outlined" />
+  //         <TextField
+  //           id="outlined-helperText"
+  //           label="Helper text"
+  //           defaultValue="Default Value"
+  //           helperText="Some important text"
+  //           variant="outlined"
+  //         />
+  //       </div>
+  //     </form>
+  //   </div>
+  )
 }
 export default Profile

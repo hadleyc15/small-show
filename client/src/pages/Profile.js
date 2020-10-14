98bff80c94532b19c09b73fe3cfeb2d1904d6a85
siwel20 <plthrock@gmail.com>
2020-10-13 20:26:56 -0600

    formated Profile page to include the users first, last and twitch username

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -29,80 +29,14 @@ function Profile() {
       <h2>Profile Page</h2>
       {/* <form onSubmit={handleFormSubmit}> */}
         <div className="flex-row space-between my-2">
-          <p>{user.lastName}</p>
-          <p>{user.firstName}</p>
-          <p>{user.twitchUserName}</p>
-        {/* <p>{item.name}</p>
-        <p>{name}</p>
-        <p>{firstName}</p>
-        <p>{lastName}</p> */}
-          <label htmlFor="firstName">First Name*:</label>
-          <input
-            placeholder="First"
-            name="firstName"
-            type="firstName"
-            id="firstName"
-            // onChange={handleChange}
-          />
+          <p>First Name: {user.firstName}</p>
         </div>
         <div className="flex-row space-between my-2">
-          <label htmlFor="lastName">Last Name*:</label>
-          <input
-            placeholder="Last"
-            name="lastName"
-            type="lastName"
-            id="lastName"
-            // onChange={handleChange}
-          />
-        </div>
-
-        <div className="flex-row space-between my-2">
-          <label htmlFor="userName">Username*:</label>
-          <input
-            placeholder="Username"
-            name="userName"
-            type="userName"
-            id="userName"
-            // onChange={handleChange}
-          />
+          <p>Last Name: {user.lastName}</p>
         </div>
         <div className="flex-row space-between my-2">
-          <label htmlFor="twitchUserName">Twitch Username:</label>
-          <input
-            placeholder="Twitch Username"
-            name="twitchUserName"
-            type="twitchUserName"
-            id="twitchUserName"
-            // onChange={handleChange}
-          />
+        <p>Twitch Username: {user.twitchUserName}</p>
         </div>
-
-        <div className="flex-row space-between my-2">
-          <label htmlFor="email">Email*:</label>
-          <input
-            placeholder="youremail@test.com"
-            name="email"
-            type="email"
-            id="email"
-            // onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="pwd">Password*:</label>
-          <input
-            placeholder="Password"
-            name="password"
-            type="password"
-            id="pwd"
-            // onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row flex-end">
-          <button type="submit">
-            Submit
-          </button>
-        </div>
-      {/* </form> */}
     </div>
   // return (
   //   <div className="container">

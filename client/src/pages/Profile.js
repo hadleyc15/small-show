73998478f2b72867bd204338c0a288b4b2b06237
siwel20 <plthrock@gmail.com>
2020-10-13 20:54:28 -0600

    cleaned up the code for the profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
 import { Link } from "react-router-dom";
 import {QUERY_USER} from "../utils/queries";
@@ -47,60 +46,6 @@ function Profile() {
           <p>**More features coming soon!**</p>
         </div>
     </div>
-  // return (
-  //   <div className="container">
-  //     <form className={classes.root} noValidate autoComplete="off">
-  //       <div>
-  //         <TextField
-  //           required
-  //           id="outlined-required"
-  //           label="Required"
-  //           defaultValue="Placeholder Username"
-  //           variant="outlined"
-  //         />
-  //         <TextField
-  //           disabled
-  //           id="outlined-disabled"
-  //           label="Disabled"
-  //           defaultValue="Hello World"
-  //           variant="outlined"
-  //         />
-  //         <TextField
-  //           id="outlined-password-input"
-  //           label="Password"
-  //           type="password"
-  //           autoComplete="current-password"
-  //           variant="outlined"
-  //         />
-  //         <TextField
-  //           id="outlined-read-only-input"
-  //           label="Read Only"
-  //           defaultValue="Do we need a read only text box?"
-  //           InputProps={{
-  //             readOnly: true,
-  //           }}
-  //           variant="outlined"
-  //         />
-  //         <TextField
-  //           id="outlined-number"
-  //           label="Number"
-  //           type="number"
-  //           InputLabelProps={{
-  //             shrink: true,
-  //           }}
-  //           variant="outlined"
-  //         />
-  //         <TextField id="outlined-search" label="Search field" type="search" variant="outlined" />
-  //         <TextField
-  //           id="outlined-helperText"
-  //           label="Helper text"
-  //           defaultValue="Default Value"
-  //           helperText="Some important text"
-  //           variant="outlined"
-  //         />
-  //       </div>
-  //     </form>
-  //   </div>
   )
 }
 export default Profile

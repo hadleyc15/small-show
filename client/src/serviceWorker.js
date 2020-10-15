eccca0ecca810144138612fc12949ffbabca98b8
Jim Hopkinson <jahopki@hotmail.com>
2020-10-15 06:52:31 -0600

    add more cache

--- client/src/serviceWorker.js
+++ client/src/serviceWorker.js
@@ -139,3 +139,29 @@ export function unregister() {
       });
   }
 }
+
+
+var CACHE_NAME = 'my-pwa-cache-v1';
+var urlsToCache = [
+  '/',
+  '/index.css',
+  '../public/index.html'
+];
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        // Open a cache and cache our files
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log(event.request.url);
+  event.respondWith(
+      caches.match(event.request).then(function(response) {
+          return response || fetch(event.request);
+      })
+  );
+});
